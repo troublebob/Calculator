94b4d8f343b52fa8efb334b096a4ff47e9c0cfe8
Rob <Robert.Foran@Ucdconnect.ie>
2014-04-28 16:21:46 +0100

    Draw Graphics Lib

--- src/TestCode.java
+++ src/TestCode.java
@@ -1,86 +1,35 @@
 import Formulator.*;
 
 import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
+import java.lang.reflect.Array;
 import java.util.regex.*;
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class TestCode {
 
 	public static void main(String[] args) {
-		String input="3^2^4";
-		//String input ="x";
-		FormulaElement result = FormulaElement.parseFormula(input);
-		//System.out.println(result);
-		//System.out.println(result.isFullyGrounded());
-		result.setVariableValue("x", 5);
-		System.out.println(result);
-		result.setVariableValue("y", 8);
-		System.out.println(result);
-		System.out.println(result.isFullyGrounded());
-		System.out.println(((FormulaElement)result).getValue());
-		//System.out.println(result.getArguments());
+		
+		//FormulaElement result = FormulaElement.parseFormula(input);
+		/*public static void main(String[] args) {
+      	SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+            createAndShowGui();
+         	}
+      	});*/
+		ArrayList<FormulaElement> graph = new ArrayList<FormulaElement>();
+		ArrayList<Integer> graphVals = new ArrayList<Integer>();
+		for (int i = 0; i < 16; i++) {
+			FormulaElement result = FormulaElement.parseFormula("3x+5*20");
+			result.setVariableValue("x", i);
+			graph.add(result);
+			graphVals.add((int)result.getValue());
+		}
+		
+		DrawGraph testGraph = new DrawGraph(graphVals);
+		testGraph.createAndShowGui(graphVals);
 	}
 
 }
-/*
-//Week2
-ConstantElement ce1 =new ConstantElement(1);
-ConstantElement ce2= new ConstantElement(2);
-ConstantElement ce7 = new ConstantElement(7);
-VariableElement veX = new VariableElement("X");
-VariableElement veY = new VariableElement("Y");
-
-MultipleFunctionElement mult = new MultipleFunctionElement();
-PlusFunctionElement pl = new PlusFunctionElement();
-pl.addArguments(veX);
-pl.addArguments(ce7);
-mult.addArguments(pl);
-MinusFunctionElement ml = new MinusFunctionElement();
-ml.addArguments(veX);
-ml.addArguments(ce2);
-mult.addArguments(ml);
-pl = new PlusFunctionElement();
-pl.addArguments(veX);
-pl.addArguments(ce1);
-mult.addArguments(pl);
-
-//(X+7.0)(X-2.0)(X+1.0)
-System.out.println(mult);
-
-mult = new MultipleFunctionElement();
-MultipleFunctionElement mult1 = new MultipleFunctionElement();
-mult1.addArguments(veY);
-mult1.addArguments(veY);
-mult.addArguments(mult1);
-mult1 = new MultipleFunctionElement();
-mult1.addArguments(veX);
-mult1.addArguments(veX);
-pl = new PlusFunctionElement();
-pl.addArguments(mult1);
-pl.addArguments(ce2);
-mult.addArguments(pl);		
-
-//YY(XX+2)
-System.out.println(mult);
-
-
-pl = new PlusFunctionElement();
-pl.addArguments(mult1);
-pl.addArguments(ce7);
-SineFunctionElement s = new SineFunctionElement(pl);
-
-mult1 = new MultipleFunctionElement();
-mult1.addArguments(veY);
-mult1.addArguments(veY);
-MinusFunctionElement mi = new MinusFunctionElement();
-mi.addArguments(mult1);
-mi.addArguments(ce2);
-CosineFunctionElement c= new CosineFunctionElement(mi);
-mult = new MultipleFunctionElement();
-mult.addArguments(s);
-mult.addArguments(c);
-
-//Sin(X^2)Cos(Y^2-2)
-System.out.println(mult);
-*/
